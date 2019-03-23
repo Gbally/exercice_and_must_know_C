09d7b6c7a169fac043f37212302b3cf8fa18f3c8
Gbally <guillaumepointbally@gmail.com>
2019-03-23 10:30:08 +0800

    Fibonacci number

---
+++ fibonacci/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define ITERATION 20
+
+void fibonacci(int table[]);
+
+int main(int argc, const char * argv[]) {
+    
+    int table[] = {1, 1};
+
+    for (int i = 0; i < ITERATION; i++) {
+        fibonacci(table);
+        printf("[%d, %d]\n", table[0], table[1]);
+    }
+    
+    return 0;
+}
+
+void fibonacci(int table[]) {
+    
+    int a = table[0];
+    int b = table[1];
+    
+    table[0] = b;
+    table[1] = a + b;
+}
